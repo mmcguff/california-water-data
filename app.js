12b6ccb38495d0652f93c2cda094fc153799b22a
Matthew McGuff <mmcguff@authenticity.ai>
2020-03-30 06:25:16 -0600

    add ranchsystem route with security improvements

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
+
 const express = require('express');
 const cors = require('cors');
-const rp = require('request-promise');
+const fetch = require('node-fetch');
 const CryptoJS = require('crypto-js');
 const morgan = require('morgan');
-let moment = require('moment');
-moment().format();
 const app = express();
 
-const calTestPayload = require('./cal-test.json');
-
-
-
-app.use(cors());
-app.use(morgan('combined'));
-
+let moment = require('moment');
+moment().format();
 
 function getTime(){
 	const today = moment().utcOffset(-8).format('YYYY-MM-DD');
@@ -21,42 +15,42 @@ function getTime(){
 	return {today, sixDaysAgo};
 }
 
-app.get('/californiaWaterData/:target', async function(req, res){
+app.use(cors());
+app.use(morgan('combined'));
+
+app.get('/californiaWaterData/:target', async (req, res) => {
     const target = req.params.target;
     const startDate = getTime().sixDaysAgo;
     const endDate = getTime().today;
-    const API_KEY = '2298ecc1-317c-4f19-a8d0-948ca1ae719f';
+    const API_KEY = process.env.CAL_WATER_APIKEY;
     const uri = 'http://et.water.ca.gov/api/data'
     + '?appKey=' + API_KEY 
 	+ '&targets=' + target 
 	+ '&startDate=' + startDate
 	+ '&endDate=' + endDate
 	+ '&dataItems=day-eto';
-    
-    let options = {
-        uri,
-        headers: {
-            'Accept': 'application/json'
-        },
-        json: true 
-    };
-     
-    rp(options)
-        .then(function (payload) {
-            res.send(payload);
-        })
-        .catch(function (err) {
-            console.log(err);
-            res.send(err);
-        });
 
+    const options = {
+        headers: {
+            'Accept': '*/*'
+        }
+    };
+
+    await fetch(uri, options)
+    .then(response => response.json())
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        console.log(err);
+        res.send(err);
+    })
 });
 
+app.get('/fieldClimateData', async (req, res) => {
 
-app.get('/fieldClimateData', async function(req, res){
-
-    const public_key = '8e0037389c697e256700b56601738ee7cf2de70e';
-    const private_key = '9bb981cba1243576de45e917d892c7106abf2250';
+    const public_key = process.env.FIELD_CLIMATE_PUBLIC_APIKEY;
+    const private_key = process.env.FIELD_CLIMATE_PRIVATE_APIKEY;
     const timestamp = new Date().toUTCString(); 
 
     const stationCode = '002047A7';
@@ -68,24 +62,49 @@ app.get('/fieldClimateData', async function(req, res){
     let signature = CryptoJS.HmacSHA256(content_to_sign, private_key);
     const hmac_str = "hmac " + public_key + ":" + signature;
 
-        let options = {
-            uri,
+        const options = {
+            method,
             headers: {
                 'Accept': 'application/json',
                 'Authorization': hmac_str,
                 'Request-Date': timestamp
-            },
-            json: true 
-        };
-         
-        rp(options)
-            .then(function (payload) {
-                res.send(payload);
-            })
-            .catch(function (err) {
-                console.log(err);
-                res.send(err);
-            });
+            }
+        }
+
+        await fetch(uri, options)
+        .then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.send(err);
+        })
+});
+
+app.get('/ranchSystems/:days', async (req, res) => {
+
+    const baseurl = 'https://app.ranchsystems.com';
+    const username = process.env.RANCH_SYSTEMS_USERNAME;
+    const password = process.env.RANCH_SYSTEMS_PASSWORD;
+    const requestType = 'data';
+    const rmsids = ['227985','227986','227987','227988','227989','227990'];
+    const to = new Date().getTime();
+    const from = to - req.params.days * 24 * 60 * 60 * 1000
+
+    const request = `/rsapp15/jsp/rsjson.jsp?uname=${username}&pword=${password}&reqtype=${requestType}&rmsids=${rmsids.join(',')}&from=${from}&to=${to}`
+
+    const uri = baseurl + request; 
+
+    await fetch(uri)
+    .then(response => response.json())
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        console.log(err);
+        res.send(err);
+    })
 });
 
 const port = process.env.PORT || 3000;
