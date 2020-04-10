0407f8ec2904f34900845543d6cf7f6d0144bd4e
Matthew McGuff <mmcguff@authenticity.ai>
2020-04-10 12:14:20 -0600

    add transformation logic for ranch systems

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-
+//package dependencies
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const CryptoJS = require('crypto-js');
 const morgan = require('morgan');
 const app = express();
-
 let moment = require('moment');
 moment().format();
 
-function getTime(){
-	const today = moment().utcOffset(-8).format('YYYY-MM-DD');
-	const sixDaysAgo = moment().utcOffset(-8).subtract(6, 'days').format('YYYY-M-DD');
-	return {today, sixDaysAgo};
-}
+//env
+const CAL_WATER_BASEURL = process.env.CAL_WATER_BASEURL;
+const CAL_WATER_APIKEY = process.env.CAL_WATER_APIKEY;
+const FIELD_CLIMATE_BASEURL = process.env.FIELD_CLIMATE_BASEURL;
+const FIELD_CLIMATE_PUBLIC_KEY = process.env.FIELD_CLIMATE_PUBLIC_APIKEY;
+const FIELD_CLIMATE_PRIVATE_KEY = process.env.FIELD_CLIMATE_PRIVATE_APIKEY;
+const RANCH_SYSTEMS_BASEURL = process.env.RANCH_SYSTEMS_BASEURL;
+const RANCH_SYSTEMS_USERNAME = process.env.RANCH_SYSTEMS_USERNAME;
+const RANCH_SYSTEMS_PASSWORD = process.env.RANCH_SYSTEMS_PASSWORD;
+
+//custom utlis
+const utils = require('./helper/utils');
 
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 
 app.get('/californiaWaterData/:target', async (req, res) => {
+    
     const target = req.params.target;
-    const startDate = getTime().sixDaysAgo;
-    const endDate = getTime().today;
-    const API_KEY = process.env.CAL_WATER_APIKEY;
-    const uri = 'http://et.water.ca.gov/api/data'
-    + '?appKey=' + API_KEY 
-	+ '&targets=' + target 
-	+ '&startDate=' + startDate
-	+ '&endDate=' + endDate
-	+ '&dataItems=day-eto';
+    const startDate = utils.getTime().sixDaysAgo;
+    const endDate = utils.getTime().today;
+    const uri = `${CAL_WATER_BASEURL}?appKey=${CAL_WATER_APIKEY}&targets=${target}&startDate=${startDate}&endDate=${endDate}&dataItems=day-eto`;
 
     const options = {
         headers: {
@@ -49,18 +50,15 @@ app.get('/californiaWaterData/:target', async (req, res) => {
 
 app.get('/fieldClimateData', async (req, res) => {
 
-    const public_key = process.env.FIELD_CLIMATE_PUBLIC_APIKEY;
-    const private_key = process.env.FIELD_CLIMATE_PRIVATE_APIKEY;
     const timestamp = new Date().toUTCString(); 
-
     const stationCode = '002047A7';
     const method = 'GET';
     const request = `/data/normal/${stationCode}/hourly/last/21d`;
-    const uri = `https://api.fieldclimate.com/v1/data/normal/${stationCode}/hourly/last/21d`
+    const uri = `${FIELD_CLIMATE_BASEURL}/${stationCode}/hourly/last/21d`
 
-    const content_to_sign = method + request + timestamp + public_key;
-    let signature = CryptoJS.HmacSHA256(content_to_sign, private_key);
-    const hmac_str = "hmac " + public_key + ":" + signature;
+    const content_to_sign = method + request + timestamp + FIELD_CLIMATE_PUBLIC_KEY;
+    let signature = CryptoJS.HmacSHA256(content_to_sign, FIELD_CLIMATE_PRIVATE_KEY);
+    const hmac_str = "hmac " + FIELD_CLIMATE_PUBLIC_KEY + ":" + signature;
 
         const options = {
             method,
@@ -84,22 +82,16 @@ app.get('/fieldClimateData', async (req, res) => {
 
 app.get('/ranchSystems/:days', async (req, res) => {
 
-    const baseurl = 'https://app.ranchsystems.com';
-    const username = process.env.RANCH_SYSTEMS_USERNAME;
-    const password = process.env.RANCH_SYSTEMS_PASSWORD;
     const requestType = 'data';
     const rmsids = ['227985','227986','227987','227988','227989','227990', '227975'];
     const to = new Date().getTime();
     const from = to - req.params.days * 24 * 60 * 60 * 1000
-
-    const request = `/rsapp15/jsp/rsjson.jsp?uname=${username}&pword=${password}&reqtype=${requestType}&rmsids=${rmsids.join(',')}&from=${from}&to=${to}`
-
-    const uri = baseurl + request; 
-
+    const uri = `${RANCH_SYSTEMS_BASEURL}/rsapp15/jsp/rsjson.jsp?uname=${RANCH_SYSTEMS_USERNAME}&pword=${RANCH_SYSTEMS_PASSWORD}&reqtype=${requestType}&rmsids=${rmsids.join(',')}&from=${from}&to=${to}`; 
+    
     await fetch(uri)
     .then(response => response.json())
     .then(data => {
-        res.send(data);
+        res.send(utils.ranchSystemsTransform(data));
     })
     .catch(err => {
         console.log(err);
---
+++ helper/utils.js
@@ -0,0 +1,52 @@
+const internals = {};
+const _ = require('lodash');
+let moment = require('moment');
+moment().format();
+
+internals.getTime = () => {
+    const today = moment().utcOffset(-8).format('YYYY-MM-DD');
+    const sixDaysAgo = moment().utcOffset(-8).subtract(6, 'days').format('YYYY-M-DD');
+    return {today, sixDaysAgo};
+}
+
+internals.ranchSystemsTransform = sourcePayload => {
+    
+    const targetDateStringFormat = 'YYYY/MM/DD hh:mm:ss';
+    const targetTimeStringFormat = 'h:mm a';
+    const data = sourcePayload.data;
+
+    let transformedPayload = [];
+
+    const _4inProbeId = 227985;
+    const _12inProbeId = 227986;
+    const _24inProbeId = 227987;
+    const _36inProbeId = 227988;
+    const _48inProbeId = 227989;
+    const _60inProbeId = 227990;
+    const _pressueId = 227975;
+
+    const _4inProbeIndexLocation = _.findIndex(data, ['id', _4inProbeId]);
+    const _12inProbeIndexLocation = _.findIndex(data, ['id', _12inProbeId]);
+    const _24inProbeIndexLocation = _.findIndex(data, ['id', _24inProbeId]);
+    const _36inProbeIndexLocation = _.findIndex(data, ['id', _36inProbeId]);
+    const _48inProbeIndexLocation = _.findIndex(data, ['id', _48inProbeId]);
+    const _60inProbeIndexLocation = _.findIndex(data, ['id', _60inProbeId]);
+    const _pressureLocation = _.findIndex(data, [ 'id',  _pressueId]);
+
+    for (let i = 0; i < data[_4inProbeIndexLocation].rmsdata.length; i++) {
+        transformedPayload.push({
+            "date": moment(new Date(data[_4inProbeIndexLocation].rmsdata[i].x)).format(targetDateStringFormat),
+            "10_X_X_4_time": moment(new Date(data[_4inProbeIndexLocation].rmsdata[i].x)).format(targetTimeStringFormat),
+            "4_in_soil_moisture": data[_4inProbeIndexLocation].rmsdata[i].y,
+            "8_in_soil_moisture": data[_12inProbeIndexLocation].rmsdata[i].y,
+            "24_in_soil_moisture": data[_24inProbeIndexLocation].rmsdata[i].y,
+            "36_in_soil_moisture": data[_36inProbeIndexLocation].rmsdata[i].y,
+            "48_in_soil_moisture": data[_48inProbeIndexLocation].rmsdata[i].y,
+            "60_in_soil_moisture": data[_60inProbeIndexLocation].rmsdata[i].y,
+            "Pressure_0-100psi": data[_pressureLocation].rmsdata[i].y
+        })
+    }
+    return transformedPayload;
+} 
+
+module.exports = internals;
