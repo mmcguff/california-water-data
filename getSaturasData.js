6c5c7c10fb3d609639d220a2e1e47d7bb25eb531
Matthew McGuff <mmcguff@byui.edu>
2020-07-11 08:21:39 -0600

    Change some configs to fix Saturas Cron.

--- getSaturasData.js
+++ getSaturasData.js
@@ -8,12 +8,11 @@ const utils = require("./helper/utils");
   //browser setup
   const browser = await puppeteer.launch({ 
     headless: true,
-    defaultViewport: null,
+    defaultViewport: { width: 1920, height: 937 },
     devtools: true,
-    args: ['--no-sandbox', '--disable-dev-shm-usage'] 
+    args: ['--no-sandbox'] 
   });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 937 });
   
   //enable console logging
   await utils.enableVerboseBrowserLogging(page);
