a06f71e5c61658a816200ae1da4547368790dfb5
Matthew McGuff <mmcguff@byui.edu>
2021-01-20 20:17:52 -0700

    add basic login logic

---
+++ cron-atn.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const utils = require('./helpers/utils');
+
+
+(async () => {
+
+const browser = await puppeteer.launch({ 
+    headless: false,
+    defaultViewport: { width: 1920, height: 937 },
+    devtools: true,
+    args: ['--no-sandbox'] 
+    });
+const [page] = await browser.pages();
+await page.goto(process.env.ATN_URL);
+await page.waitForSelector('input[name=j_username]');
+await page.focus('input[name=j_username]');
+await page.keyboard.type(process.env.ATN_USERNAME);
+await page.focus('input[name=j_password]');
+await page.keyboard.type(process.env.ATN_PASSWORD);
+await page.click('input[type=submit]');
+
+
+
+})();
