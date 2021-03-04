bd612f625bdeef3b644476606d6ccbacc04b8915
Matthew McGuff <matthew.mcguff@fittedrunning.com>
2021-03-04 05:49:26 -0700

    stub out first parts of semios cron commands.

---
+++ cron-semios.js
@@ -0,0 +1,10 @@
+const semios = require('./helpers/semios-cron-utils');
+const fetch = require('node-fetch');
+const moment = require('moment');
+moment().format();
+
+const emoji = require('node-emoji');
+
+(async () => {
+    console.log(`...${emoji.get('tulip')} Semios Cron Start`)
+})();
